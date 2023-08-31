04a0f52d5e1dbee5d3a3b957ff9c7a8cb2450556
Ritvik25goyal <goyalritvik9@gmail.com>
2023-08-31 19:22:30 +0530

    fixed agents

--- Wordle_Evil_dordle_agents/solvedordle.py
+++ Wordle_Evil_dordle_agents/solvedordle.py
@@ -16,7 +16,6 @@ from play import RIGHT_PLACE, eval_guess, WRONG_PLACE, LETTER_ABSENT
 from possibilities_table import (
     array_to_integer,
     load_possibilities_table,
-    load_possibilities_table_df,
 )
 
 FIRST_GUESS_WORD = "serai"
